493b32853cf65b8723ddf435ae6b5cdf9b5949db
Geoffrey Magana <geoffreymagana3@gmail.com>
2025-09-17 12:40:12 +0000

    Added images to search results

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -18,7 +18,7 @@ export const mockMamaFuas: MamaFua[] = [
     distance: '0.5 km',
     price: 'KES 150/load',
     specialties: ['Delicate fabrics', 'Ironing', 'Same-day service'],
-    image: 'https://images.unsplash.com/photo-1580529261239-015515204555?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGFmcmljYW4lMjBsYWR5fGVufDB8fDB8fHww',
+    image: '/images/Grace Wanjiku.png',
   },
   {
     id: '2',
@@ -28,7 +28,7 @@ export const mockMamaFuas: MamaFua[] = [
     distance: '1.2 km',
     price: 'KES 120/load',
     specialties: ['Bulk laundry', 'Eco-friendly', 'Pickup & delivery'],
-    image: 'https://images.unsplash.com/photo-1601233588267-34c45b849cd9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFmcmljYW4lMjBsYWR5fGVufDB8fDB8fHww',
+    image: '/images/Mary Akinyi.png',
   },
   {
     id: '3',
@@ -38,7 +38,7 @@ export const mockMamaFuas: MamaFua[] = [
     distance: '2.1 km',
     price: 'KES 180/load',
     specialties: ['Premium care', 'Stain removal', 'Express service'],
-    image: 'https://images.unsplash.com/photo-1605988442532-581335a443b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFmcmljYW4lMjBsYWR5fGVufDB8fDB8fHww',
+    image: '/images/Janet Muthoni.png',
   },
   {
     id: '4',
@@ -48,7 +48,7 @@ export const mockMamaFuas: MamaFua[] = [
     distance: '2.5 km',
     price: 'KES 130/load',
     specialties: ['Bedding & Linens', 'Ironing'],
-    image: 'https://images.unsplash.com/photo-1599387737527-88c671610996?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWZyaWNhbiUyMGxhZHl8ZW58MHx8MHx8fDA%3D',
+    image: '/images/Esther Nyambura.png',
   },
    {
     id: '5',
@@ -58,6 +58,6 @@ export const mockMamaFuas: MamaFua[] = [
     distance: '3.1 km',
     price: 'KES 160/load',
     specialties: ['Same-day service', 'Stain removal', 'Pickup & delivery'],
-    image: 'https://plus.unsplash.com/premium_photo-1664298175829-651278359438?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhbiUyMGxhZHl8ZW58MHx8MHx8fDA%3D',
+    image: '/images/Beatrice Atieno.png',
   },
 ];
